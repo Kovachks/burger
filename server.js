b239dfe000da8626e1d1ea0e3cbf4200aa4cebb7
Keith Kovach <kovachks90@gmail.com>
2017-09-07 17:28:23 -0400

    server.js file updated

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ var port = 3000;
 
 var app = express();
 
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use(medhodOverride("_method"));
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controller.js")
+
+app.use("/", routes);
+
+app.listen(port);
